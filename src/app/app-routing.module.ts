16e75c3b0f28a680d3e771cb172300e5f378356f
Johnny Perdomo <johnnyperdomo7@gmail.com>
2021-03-23 11:01:23 -0400

    generate skeleton components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,65 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ItemComponent } from './item/item.component';
+import { PageNotFoundComponent } from './misc/page-not-found/page-not-found.component';
+import { AccountComponent } from './settings/account/account.component';
+import { BillingComponent } from './settings/billing/billing.component';
+import { IntegrationsComponent } from './settings/integrations/integrations.component';
+import { SettingsComponent } from './settings/settings.component';
 
+//TODO: add pipe guards for logged in states
+const routes: Routes = [
+  //Auth
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+  },
 
-const routes: Routes = [];
+  { path: 'dashboard', component: DashboardComponent },
+
+  //Main
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+
+  {
+    path: 'item/new',
+    component: ItemComponent,
+  }, //new item
+
+  {
+    path: 'item/:id/edit',
+    component: ItemComponent,
+  }, //edit item
+
+  //Settings
+  {
+    path: 'settings',
+    redirectTo: 'settings/integrations',
+  }, //redirects to settings/integrations
+
+  {
+    path: 'settings',
+    component: SettingsComponent,
+    children: [
+      { path: 'integrations', component: IntegrationsComponent },
+      { path: 'account', component: AccountComponent },
+      { path: 'billing', component: BillingComponent },
+    ],
+  },
+
+  //404 - Page not found
+  { path: '404', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '/404' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,20 @@ import { environment } from 'src/environments/environment';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireAuthModule } from '@angular/fire/auth';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ItemComponent } from './item/item.component';
+import { PageNotFoundComponent } from './misc/page-not-found/page-not-found.component';
+import { AccountComponent } from './settings/account/account.component';
+import { BillingComponent } from './settings/billing/billing.component';
+import { IntegrationsComponent } from './settings/integrations/integrations.component';
+import { SettingsComponent } from './settings/settings.component';
 
 firebase.initializeApp(environment.firebaseConfig);
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LoginComponent, SignupComponent, DashboardComponent, ItemComponent, PageNotFoundComponent, AccountComponent, BillingComponent, IntegrationsComponent, SettingsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
