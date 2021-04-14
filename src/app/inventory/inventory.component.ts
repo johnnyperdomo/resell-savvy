9f97ede0f55c27f99e0cc6f3cbc26b9ff9046cad
Johnny Perdomo <johnnyperdomo7@gmail.com>
2021-04-14 14:49:03 -0400

    add stats value to get overview of inventory; parse price/copst values with currency.js

--- src/app/inventory/inventory.component.ts
+++ src/app/inventory/inventory.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as currency from 'currency.js';
 
 @Component({
   selector: 'app-inventory',
@@ -8,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class InventoryComponent implements OnInit {
   //LATER: paginate data so we don't pull all the items at once, as this can cause alot of reads which can become expensive later on; if i do this i have to add custom elastic search functionality, I also have to queryable data most likely from google bigquery to get 'stats' values
   //TODO: make sure to manipulate date values
+  //LATER: make sure that currencies are parsed correctly before manipulating: 17 => $17.00
   items = [
     {
       itemId: 'item123',
@@ -27,9 +29,9 @@ export class InventoryComponent implements OnInit {
       sku: null,
       imageUrl:
         'https://dashkit.goodthemes.co/assets/img/avatars/products/product-1.jpg',
-      status: 'sold',
-      price: '17.76',
-      cost: '2.56',
+      status: 'active',
+      price: '17',
+      cost: '2',
       date: 'Jan 17, 2020',
       marketplaces: ['poshmark', 'mercari', 'etsy', 'grailed'],
     },
@@ -39,7 +41,7 @@ export class InventoryComponent implements OnInit {
       sku: null,
       imageUrl:
         'https://dashkit.goodthemes.co/assets/img/avatars/products/product-2.jpg',
-      status: 'active',
+      status: 'sold',
       price: '234.76',
       cost: '56.56',
       date: 'Jan 19, 2020',
@@ -49,11 +51,61 @@ export class InventoryComponent implements OnInit {
 
   filteredItems: any;
 
-  constructor() {
+  //stats
+  itemsInInventory: number;
+  activeListings: number;
+  soldItems: number;
+  totalSales: number;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.parseCurrencyValues();
     this.filteredItems = this.items;
+
+    //stats
+    this.itemsInInventory = this.items.length;
+    this.activeListings = this.getActiveListings();
+    this.soldItems = this.getSoldItems();
+    this.totalSales = this.getTotalSales();
   }
 
-  ngOnInit(): void {}
+  parseCurrencyValues() {
+    this.items.map((item) => {
+      if (item.price) {
+        //parse prices currencies: 17 => 17.00
+        item.price = currency(item.price).toString();
+      }
+      if (item.cost) {
+        //parse cost currencies: 17 => 17.00
+        item.cost = currency(item.cost).toString();
+      }
+    });
+  }
+
+  getActiveListings() {
+    return this.items.filter((item) => {
+      return item.status == 'active';
+    }).length;
+  }
+
+  getSoldItems() {
+    return this.items.filter((item) => {
+      return item.status == 'sold';
+    }).length;
+  }
+
+  getTotalSales() {
+    const soldItems = this.items.filter((item) => {
+      return item.status == 'sold';
+    });
+
+    return soldItems.reduce((n, { price }) => {
+      //add; currency math
+      const currencyVal = currency(n).add(price).toString();
+      return Number(currencyVal);
+    }, 0);
+  }
 
   onSearchKey(event) {
     const titleInput = event.target.value.toLowerCase();
