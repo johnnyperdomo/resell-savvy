42a4d66664d001926b0c46d58873d70de7050725
Johnny Perdomo <johnnyperdomo7@gmail.com>
2021-04-14 10:50:56 -0400

    update

--- src/app/misc/pricing-modal/pricing-modal.component.ts
+++ src/app/misc/pricing-modal/pricing-modal.component.ts
@@ -12,11 +12,15 @@ import { loadStripe } from '@stripe/stripe-js';
 export class PricingModalComponent implements OnInit {
   isLoading: boolean;
 
+  isSubscribed: Boolean; //TODO: hide or show alert based on if user is subscribed or not.
+
   docRef: DocumentReference;
 
   constructor(private db: AngularFirestore, private auth: AngularFireAuth) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    //TODO: is subscribed, check if exists for new user, or check if they can find any active.
+  }
 
   async onStartProPlan() {
     this.isLoading = true;
@@ -50,63 +54,63 @@ export class PricingModalComponent implements OnInit {
     });
   }
 
-  async onStartBusinessPlan() {
-    this.isLoading = true;
-    this.docRef = await this.db
-      .collection('users')
-      .doc((await this.auth.currentUser).uid)
-      .collection('checkout_sessions')
-      .add({
-        price: 'price_1IYFKKKWbAZBgyTQ0jCIzolD',
-        success_url: window.location.origin,
-        cancel_url: window.location.origin,
-      });
-    // Wait for the CheckoutSession to get attached by the extension
-    this.docRef.onSnapshot(async (snap) => {
-      const { error, sessionId } = snap.data();
-      if (error) {
-        // Show an error to your customer and
-        // inspect your Cloud Function logs in the Firebase console.
-        alert(`An error occured: ${error.message}`);
-      }
-      if (sessionId) {
-        // We have a session, let's redirect to Checkout
-        // Init Stripe
-        const stripe = await loadStripe(
-          'pk_test_51HtaF3KWbAZBgyTQXKMAp5h3nc1kmAWbi5vBcp14AmrqXR8XWYKXwIPhSZoOPiZBKRkl9Id6wi6nHi8p5NApRR0000oRMZr5Zl'
-        );
-        stripe.redirectToCheckout({ sessionId });
-      }
-    });
-  }
+  // async onStartBusinessPlan() {
+  //   this.isLoading = true;
+  //   this.docRef = await this.db
+  //     .collection('users')
+  //     .doc((await this.auth.currentUser).uid)
+  //     .collection('checkout_sessions')
+  //     .add({
+  //       price: 'price_1IYFKKKWbAZBgyTQ0jCIzolD',
+  //       success_url: window.location.origin,
+  //       cancel_url: window.location.origin,
+  //     });
+  //   // Wait for the CheckoutSession to get attached by the extension
+  //   this.docRef.onSnapshot(async (snap) => {
+  //     const { error, sessionId } = snap.data();
+  //     if (error) {
+  //       // Show an error to your customer and
+  //       // inspect your Cloud Function logs in the Firebase console.
+  //       alert(`An error occured: ${error.message}`);
+  //     }
+  //     if (sessionId) {
+  //       // We have a session, let's redirect to Checkout
+  //       // Init Stripe
+  //       const stripe = await loadStripe(
+  //         'pk_test_51HtaF3KWbAZBgyTQXKMAp5h3nc1kmAWbi5vBcp14AmrqXR8XWYKXwIPhSZoOPiZBKRkl9Id6wi6nHi8p5NApRR0000oRMZr5Zl'
+  //       );
+  //       stripe.redirectToCheckout({ sessionId });
+  //     }
+  //   });
+  // }
 
-  async onStartEnterprisePlan() {
-    this.isLoading = true;
-    this.docRef = await this.db
-      .collection('users')
-      .doc((await this.auth.currentUser).uid)
-      .collection('checkout_sessions')
-      .add({
-        price: 'price_1IYFMaKWbAZBgyTQ3UYzrmPE',
-        success_url: window.location.origin,
-        cancel_url: window.location.origin,
-      });
-    // Wait for the CheckoutSession to get attached by the extension
-    this.docRef.onSnapshot(async (snap) => {
-      const { error, sessionId } = snap.data();
-      if (error) {
-        // Show an error to your customer and
-        // inspect your Cloud Function logs in the Firebase console.
-        alert(`An error occured: ${error.message}`);
-      }
-      if (sessionId) {
-        // We have a session, let's redirect to Checkout
-        // Init Stripe
-        const stripe = await loadStripe(
-          'pk_test_51HtaF3KWbAZBgyTQXKMAp5h3nc1kmAWbi5vBcp14AmrqXR8XWYKXwIPhSZoOPiZBKRkl9Id6wi6nHi8p5NApRR0000oRMZr5Zl'
-        );
-        stripe.redirectToCheckout({ sessionId });
-      }
-    });
-  }
+  // async onStartEnterprisePlan() {
+  //   this.isLoading = true;
+  //   this.docRef = await this.db
+  //     .collection('users')
+  //     .doc((await this.auth.currentUser).uid)
+  //     .collection('checkout_sessions')
+  //     .add({
+  //       price: 'price_1IYFMaKWbAZBgyTQ3UYzrmPE',
+  //       success_url: window.location.origin,
+  //       cancel_url: window.location.origin,
+  //     });
+  //   // Wait for the CheckoutSession to get attached by the extension
+  //   this.docRef.onSnapshot(async (snap) => {
+  //     const { error, sessionId } = snap.data();
+  //     if (error) {
+  //       // Show an error to your customer and
+  //       // inspect your Cloud Function logs in the Firebase console.
+  //       alert(`An error occured: ${error.message}`);
+  //     }
+  //     if (sessionId) {
+  //       // We have a session, let's redirect to Checkout
+  //       // Init Stripe
+  //       const stripe = await loadStripe(
+  //         'pk_test_51HtaF3KWbAZBgyTQXKMAp5h3nc1kmAWbi5vBcp14AmrqXR8XWYKXwIPhSZoOPiZBKRkl9Id6wi6nHi8p5NApRR0000oRMZr5Zl'
+  //       );
+  //       stripe.redirectToCheckout({ sessionId });
+  //     }
+  //   });
+  // }
 }
