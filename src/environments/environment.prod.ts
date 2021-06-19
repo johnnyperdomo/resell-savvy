2cd20bdb5d1c6d8b21b6473419196db697124ffe
Johnny Perdomo <johnnyperdomo7@gmail.com>
2021-06-18 21:30:04 -0400

    add production environment keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,21 @@
 export const environment = {
   production: true,
-
+  firebaseConfig: {
+    apiKey: 'AIzaSyDtCBELRoPX5_yROLsHyYjJB0ruc-M5Pjo',
+    authDomain: 'reseller-savvy.firebaseapp.com',
+    projectId: 'reseller-savvy',
+    storageBucket: 'reseller-savvy.appspot.com',
+    messagingSenderId: '377665812888',
+    appId: '1:377665812888:web:de58d14adfbaf5f9c5d3ed',
+  },
   azure: {
     itemImages: 'item-images',
     accountName: 'resellsavvyprod',
+    storagePath: 'https://resellsavvyprod.blob.core.windows.net/item-images/',
+  },
+  stripe: {
+    publishableKey:
+      'pk_live_51HtaF3KWbAZBgyTQq29b3M7tbJlqM2HhGlZU4E76P9Hnb1V9Dw82fPmtMcBobqbV1bMszJudT2wklQsQkHeKprZ500x8spOu3T',
+    priceId: 'price_1J3swhKWbAZBgyTQ8qfdJ9Pi',
   },
 };
-
-//TODO: production stripe keys/price-ids
-//TODO: create production version of firebase
